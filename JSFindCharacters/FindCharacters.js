238833165b723e08080eb2648a85cb06d197fb17
Justine Elbuhel <nineplusfour.13@gmail.com>
2024-01-22 12:53:11 -0800

    Solution

--- JSFindCharacters/FindCharacters.js
+++ JSFindCharacters/FindCharacters.js
@@ -1,29 +1,28 @@
 /**
- LeetCode link: https://leetcode.com/problems/find-words-containing-character/submissions/
- 
- You are given a 0-indexed array of strings words and a character x.
+//* LeetCode link: https://leetcode.com/problems/find-words-containing-character/submissions/
 
-Return an array of indices representing the words that contain the character x.
+//! You are given a 0-indexed array of strings words and a character x.
+
+//? Return an array of indices representing the words that contain the character x.
 
 Note that the returned array may be in any order.
 
- 
+
 Example 1:
-
-Input: words = ["leet","code"], x = "e"
-Output: [0,1]
+//! Input: words = ["leet","code"], x = "e"
+//? Output: [0,1]
 Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+
 Example 2:
-
-Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
-Output: [0,2]
+//! Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+//? Output: [0,2]
 Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
-Example 3:
 
-Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
-Output: []
+Example 3:
+//! Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+//?Output: []
 Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
- 
+
 
 Constraints:
 
@@ -35,42 +34,43 @@ words[i] consists only of lowercase English letters.
 
 function findCharacters(words,x){
 
-     // create the array to be returned
+    //Create an empty array that results can be pushed into
     let output = [];
 
-     //iterate through the incoming array of 'words'
-    for(let i = 0;){
+    //Loop through the array of words to isolate each individual word
+    for(let i = 0; i < words.length; i++){
 
-        //capture the word from the array for easier reference
-        const word = 
+        //Create a variable isolating one word from the array
+        const word = words[i];
 
-        //once you access a word from the 'words' array
-        //iterate through the word itself to get access to each character
-        for(let j = 0;){
+        //Console log word to check if we are isolating correctly
+        // console.log(word);
 
-            //capture the character from the word for easier reference
-            let char = 
+        //Loop through characters for the word isolated
+        for(let j = 0; j < word.length; j++){
+
+            //Create a variable for each character
+            let char = word[j]
         
-            // compare 'char' it to 'x' and see if they are equal
-            if(){
-            
-                //if the characters are equal
-                //add the current index of the 'words' array to the output array
-                 output.push()
+            //Use an if statement to see if the character matches the parameter that is x(//* x = "a")
+            if(char === x){
 
-                 //in case of a word having multiples of a letter in a word
-                    //break out of this for loop to continue onto the next word in the 'words' array
-                 break;
-                
+                //If the character matches the parameter, push the index of word into our ouput array
+                output.push(words.indexOf(word))
+                break;  
             }
         }
-       
+    }
+
+    //If there are no matches and the output array has a length of 0, return no match message
+    if(output.length == 0){
+        output = "There are no matches"
     }
     return output;
 }
 
 
-console.log(findCharacters(["abc","bcd","aaa","cbc"], "a"))
+console.log(findCharacters(["giraffe","tears","crying","cbc"], "c"))
 
 // to run javascript you must first be in the correct folder
  //then you will type  " node 'nameOfFile.js' " in the console
